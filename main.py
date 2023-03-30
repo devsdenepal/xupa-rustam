f053d7830b1c29e51e737f766e521d9db33f2d5e
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-03-30 11:12:54 +0000

    done

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ proxies = {
 "http" : "43.239.155.130:32650",
 "https":"186.159.6.163:1994"
 }
+params ={
+    'user_name':'gautam',
+    'user_pass':'123456789'
+}
 a_url = 'https://jsonip.com/'
-response = requests.get('https://jsonip.com',proxies=proxies,timeout=8)
+response = requests.get('https://',params=params,proxies=proxies,timeout=8)
 print(response.content)
